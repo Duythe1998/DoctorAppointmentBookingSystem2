7edfe346ae1998b92cdd542c47b8f92fac23d964
Nguyen Duy The <duythe.1998@gmail.com>
2021-03-18 22:27:23 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,51 @@
+import { AdminPatientComponent } from './admin/admin-patient/admin-patient.component';
+import { AdminSpecialitiesComponent } from './admin/admin-specialities/admin-specialities.component';
+import { AdminAppointmentsComponent } from './admin/admin-appointments/admin-appointments.component';
+import { AdminComponent } from './admin/admin.component';
+import { InvoicesComponent } from './invoices/invoices.component';
+import { DoctorDashboardComponent } from './doctor-dashboard/doctor-dashboard.component';
+import { BookingDoctorComponent } from './booking-doctor/booking-doctor.component';
+import { SearchDoctorComponent } from './search-doctor/search-doctor.component';
+import { LoginComponent } from './user/login/login.component';
+import { ReviewComponent } from './review/review.component';
+import { MypatientsComponent } from './mypatients/mypatients.component';
+import { AppointmentsComponent } from './appointments/appointments.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminDoctorsComponent } from './admin/admin-doctors/admin-doctors.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'index', component: HomeComponent },
+  { path: 'my-patients', component: MypatientsComponent },
+  { path: 'appointments', component: AppointmentsComponent },
+  { path: 'review-doctor', component: ReviewComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'search-doctor', component: SearchDoctorComponent },
+  { path: 'booking-doctor', component: BookingDoctorComponent },
+  { path: 'doctor-dashboard', component: DoctorDashboardComponent },
+  { path: 'invoices', component: InvoicesComponent },
+  { path: 'admin', component: AdminComponent ,
+    children : [
+        {
+          path : 'appointments' , component: AdminAppointmentsComponent
+        }, {
+          path : 'specialties' , component: AdminSpecialitiesComponent
+        },
+        {
+          path : 'doctors' , component: AdminDoctorsComponent
+        },
+        {
+          path : 'patient' , component: AdminPatientComponent
+        }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/index',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
